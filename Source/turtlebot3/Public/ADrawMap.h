afc07fd827c96ed4ff3a50f50c64e3c27eec1f7c
kuriatsu <kuriatsubayashi712@gmail.com>
2024-09-11 17:12:07 +0900

    finish build ADrawMap

--- Source/turtlebot3/Public/ADrawMap.h
+++ Source/turtlebot3/Public/ADrawMap.h
@@ -4,11 +4,32 @@
 
 #include "CoreMinimal.h"
 #include "GeometryActors/GeneratedDynamicMeshActor.h"
+#include "GeometryScript/MeshPrimitiveFunctions.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetStringLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "XmlParser.h"
 #include "ADrawMap.generated.h"
 
-#include <libxml/xmlmemory.h>
-#include <libxml/parser.h>
+// #include <libxml/xmlmemory.hpp>
+// #include <libxml/parser.hpp>
 
+USTRUCT(BlueprintType)
+struct TURTLEBOT3_API FLLNode {
+    GENERATED_BODY()
+    public:
+        FVector Location;
+        float Lat;
+        float Lon;
+};
+
+USTRUCT(BlueprintType)
+struct TURTLEBOT3_API FLLWay {
+    GENERATED_BODY()
+    public:
+        TArray<FString> Nds;
+        FString Type;
+};
 /**
  * 
  */
@@ -18,12 +39,22 @@ class TURTLEBOT3_API AADrawMap : public AGeneratedDynamicMeshActor
 	GENERATED_BODY()
 
     public:
+        AADrawMap();
+
         UFUNCTION(BlueprintCallable, Category="CPPBlueprintFunctionLibrary")
-            static void ParseLanelet2();
+            void ParseLanelet2();
+
+        UFUNCTION(BlueprintCallable, Category="CPPBlueprintFunctionLibrary")
+            void BuildMap();
+	
+        UPROPERTY(EditAnywhere)
+            TMap<FString, FLLWay> _way_map;
+
+        UPROPERTY(EditAnywhere)
+            TMap<FString, FLLNode> _nd_map;
 	
     private:
-
-	
-	
+        static FTransform MakeTransform(const FVector &location, const FRotator &rotation, const FVector &scale); 
 	
 };
+
